5ff3fe2b2d0128be4d1a2c8ef3cf95a8af123175
Mattias Ubbesen <mu222cu@student.lnu.se>
2024-11-21 23:12:14 +0100

    REFACTOR: Move the creation of questions to test setup in beforeEach block.

--- src/tests/QuestionBank.test.ts
+++ src/tests/QuestionBank.test.ts
@@ -2,28 +2,36 @@ import { Question } from '../Question'
 import { QuestionBank } from '../QuestionBank'
 
 describe('QuestionBank', () => {
-  it('should return a random question from the bank', () => {
-    const question1 = new Question(
+  let question1: Question
+  let question2: Question
+  let question3: Question
+  let questionBank: QuestionBank
+
+  beforeEach(() => {
+
+    question1 = new Question(
       'What does the term "hoisting" mean in JavaScript?',
       ['Variable declaration', 'Loop optimization', 'Runtime scope'],
       'Variable declaration'
     )
-    const question2 = new Question(
+    question2 = new Question(
       'Which HTTP status code is used when a resource is successfully created?',
       ['200', '201', '204'],
       '201'
     )
-    const question3 = new Question(
+    question3 = new Question(
       'In SOLID design principles, what does the `S` stand for?',
       ['Single responsibility', 'Simple design', 'System dependency'],
       'Single responsibility'
     )
 
-    const questionBank = new QuestionBank()
+    questionBank = new QuestionBank()
     questionBank.addQuestion(question1)
     questionBank.addQuestion(question2)
     questionBank.addQuestion(question3)
+  })
 
+  it('should return a random question from the bank', () => {
     const randomQuestion = questionBank.getRandomQuestion()
     expect([question1, question2, question3]).toContain(randomQuestion)
   })
