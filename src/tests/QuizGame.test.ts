60ef692758b325946ca204392f930f40acf65ae3
Mattias Ubbesen <mu222cu@student.lnu.se>
2024-11-22 00:34:49 +0100

    REFACTOR: Use const for options and correct answers to be able to pass to SUT methods.

--- src/tests/QuizGame.test.ts
+++ src/tests/QuizGame.test.ts
@@ -7,17 +7,21 @@ describe('QuizGame', () => {
   let question2: Question
   let questionBank: QuestionBank
   let quizGame: QuizGame
+  const options1 = ['Variable declaration', 'Loop optimization', 'Runtime scope']
+  const correctAnswer1 = 'Variable declaration'
+  const options2 = ['200', '201', '204']
+  const correctAnswer2 = '201'
 
   beforeEach(() => {
     question1 = new Question(
       'What does the term "hoisting" mean in JavaScript?',
-      ['Variable declaration', 'Loop optimization', 'Runtime scope'],
-      'Variable declaration'
+      options1,
+      correctAnswer1
     )
     question2 = new Question(
       'Which HTTP status code is used when a resource is successfully created?',
-      ['200', '201', '204'],
-      '201'
+      options2,
+      correctAnswer2
     )
 
     questionBank = new QuestionBank()
