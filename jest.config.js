a054e01d97623e104b7072565fd3cf9a03402ad1
Mattias Ubbesen <mu222cu@student.lnu.se>
2024-11-21 18:42:33 +0100

    Chore: Update jest config.

--- jest.config.js
+++ jest.config.js
@@ -2,14 +2,9 @@ export default {
   preset: 'ts-jest', // Use ts-jest to transform TypeScript files
   testEnvironment: 'node', // Set the test environment
   transform: {
-    '^.+\\.tsx?$': 'ts-jest', // Use ts-jest for .ts/.tsx files
+    '^.+\\.tsx?$': ['ts-jest', { useESM: true }], // Process .ts and .tsx files with ts-jest
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'], // Recognize file extensions
   extensionsToTreatAsEsm: ['.ts'], // Treat .ts files as ES modules
-  globals: {
-    'ts-jest': {
-      useESM: true, // Enable ECMAScript module support in ts-jest
-    },
-  },
   testMatch: ['**/tests/**/*.test.ts'], // Locate test files
 }
