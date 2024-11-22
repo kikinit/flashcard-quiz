7bf69ae629a880e496f68c5baa8f68332fabd6f9
Mattias Ubbesen <mu222cu@student.lnu.se>
2024-11-22 01:03:32 +0100

    GREEN: Implement checkAnswer method and track currentQuestion in QuizGame.

--- src/QuizGame.ts
+++ src/QuizGame.ts
@@ -3,12 +3,21 @@ import { QuestionBank } from './QuestionBank'
 
 export class QuizGame {
   private questionBank: QuestionBank
+  private currentQuestion: Question | null = null
 
   constructor(questionBank: QuestionBank) {
     this.questionBank = questionBank
   }
 
   public getNextQuestion(): Question {
-    return this.questionBank.getRandomQuestion()
+    this.currentQuestion = this.questionBank.getRandomQuestion()
+    return this.currentQuestion
+  }
+
+  public checkAnswer(answer: string): boolean {
+    if (!this.currentQuestion) {
+      throw new Error('No current question to check an answer against.')
+    }
+    return this.currentQuestion.checkAnswer(answer)
   }
 }
