5dce88748b8370beb7e468aa3aa6f0383de1cce3
Mattias Ubbesen <mu222cu@student.lnu.se>
2024-11-22 21:31:08 +0100

    GREEN: Refactor QuizGame so that Scoreboard takes on full responsibility of score handling, holding final score when game ends.

--- src/QuizGame.ts
+++ src/QuizGame.ts
@@ -1,14 +1,16 @@
 import { Question } from './Question'
 import { QuestionBank } from './QuestionBank'
+import { Scoreboard } from './Scoreboard'
 import { GameState } from './GameState'
 import { NoCurrentQuestionError, GameOverError } from './errors'
 
 export class QuizGame {
+  private readonly CORRECT_ANSWER_REWARD = 1
   private questionBank: QuestionBank
   private currentQuestion: Question | null = null
-  private score: number = 0
   private gameState: GameState = GameState.PLAYING
-
+  private scoreboard: Scoreboard = new Scoreboard()
+  
   constructor(questionBank: QuestionBank) {
     this.questionBank = questionBank
   }
@@ -33,13 +35,13 @@ export class QuizGame {
     }
     const isCorrect = this.currentQuestion.checkAnswer(answer)
     if (isCorrect) {
-      this.score++
+      this.scoreboard.increaseScore(this.CORRECT_ANSWER_REWARD)
     }
     return isCorrect
   }
 
   public getScore(): number {
-    return this.score
+    return this.scoreboard.getScore()
   }
 
   public isGameOver(): boolean {
@@ -48,7 +50,7 @@ export class QuizGame {
 
   public restart(): void {
     this.gameState = GameState.PLAYING
-    this.score = 0
+    this.scoreboard.resetScore()
     this.currentQuestion = null
     this.questionBank.resetAttemptedQuestions()
   }  
