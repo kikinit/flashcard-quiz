0e2ed881112214ae87e766e88e1dcc58851afc89
Mattias Ubbesen <mu222cu@student.lnu.se>
2024-11-22 09:49:40 +0100

    REFACTOR: Utilize beforeEach for sut setup to avoid redundancy.

--- src/tests/Question.test.ts
+++ src/tests/Question.test.ts
@@ -2,40 +2,36 @@ import { Question } from '../Question'
 import { InvalidAnswerError } from '../errors'
 
 describe('Question', () => {
-  it('should create a question with text, options, and a correct answer', () => {
-    const sut = new Question('What is 2 + 2?', ['3', '4', '5'], '4')
+  let sut: Question
+  const correctAnswer = 'Paris'
 
-    expect(sut.getText()).toBe('What is 2 + 2?')
-    expect(sut.getOptions()).toEqual(['3', '4', '5'])
-  })
-
-  it('should validate a correct answer', () => {
-    const sut = new Question('What is 2 + 2?', ['3', '4', '5'], '4')
-
-    expect(sut.checkAnswer('4')).toBe(true)
-  })
-
-  it('should invalidate an incorrect answer', () => {
-    const sut = new Question('What is 2 + 2?', ['3', '4', '5'], '4')
-
-    expect(sut.checkAnswer('3')).toBe(false)
-  })
-
-  it('should throw an InvalidAnswerError if the provided answer is not in the options', () => {
-    const sut = new Question('What is 2 + 2?', ['3', '4', '5'], '4')
-  
-    expect(() => sut.checkAnswer('7')).toThrow(InvalidAnswerError)
-  })
-
-  it('should return a random letter from the correct answer as a hint', () => {
-    const correctAnswer = 'Paris'
-    const sut = new Question(
+  beforeEach(() => {
+    sut = new Question(
       'What is the capital of France?',
       ['Berlin', 'Stockholm', correctAnswer],
       correctAnswer
     )
+  })
+
+  it('should create a question with text, options, and a correct answer', () => {
+    expect(sut.getText()).toBe('What is the capital of France?')
+    expect(sut.getOptions()).toEqual(['Berlin', 'Stockholm', correctAnswer])
+  })
+
+  it('should validate a correct answer', () => {
+    expect(sut.checkAnswer(correctAnswer)).toBe(true)
+  })
+
+  it('should invalidate an incorrect answer', () => {
+    expect(sut.checkAnswer('Berlin')).toBe(false)
+  })
+
+  it('should throw an InvalidAnswerError if the provided answer is not in the options', () => {
+    expect(() => sut.checkAnswer('New York')).toThrow(InvalidAnswerError)
+  })
+
+  it('should return a random letter from the correct answer as a hint', () => {
     const hint = sut.getHint()
-  
     expect(correctAnswer.includes(hint)).toBe(true)
   })
 })
