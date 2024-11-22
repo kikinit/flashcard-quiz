b005e628b1461f51a64c9833b291afef9d5fccf1
Mattias Ubbesen <mu222cu@student.lnu.se>
2024-11-22 21:09:27 +0100

    RED: Add test to verify that passing a score to the increaseScore method increases Scoreboard score with the corresponding value.

--- src/tests/Scoreboard.test.ts
+++ src/tests/Scoreboard.test.ts
@@ -10,4 +10,9 @@ describe('Scoreboard', () => {
   it('should start with a score of 0', () => {
     expect(scoreboard.getScore()).toBe(0)
   })
+
+  it('should increase the score by a given amount', () => {
+    scoreboard.increaseScore(10)
+    expect(scoreboard.getScore()).toBe(10)
+  })
 })
